95708d4cf5aca8defe6085d4b7fe714e5ac04f14
GaneshAngadig <ganesh.xworkz@gmail.com>
2022-02-25 20:37:33 +0530

    uploading returntype

---
+++ returntype/Truck.java
@@ -0,0 +1,19 @@
+class Truck
+{
+	public static void main (String []args)
+{
+	float response = Truck.getprice();
+	System.out.println(response);
+}
+	
+
+
+	
+	static float getprice()
+{
+	float p = 54.44546446f;
+	return p;
+
+	
+}
+}
