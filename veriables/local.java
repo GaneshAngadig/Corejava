228add4e7d1f78f3c3c64a0fa8f5af8e2d778021
GaneshAngadig <ganesh.xworkz@gmail.com>
2022-02-25 20:43:43 +0530

    uploading veriables

---
+++ veriables/local.java
@@ -0,0 +1,22 @@
+class pubg
+{
+	public static void main(String []args)
+{
+	byte noOfplayers = 100;
+		System.out.println(noOfplayers);
+	short totalvehicals = 1999;
+		System.out.println(totalvehicals);
+	int noOfroads = 656565;
+		System.out.println(noOfroads);
+	long totalhouses = 152324222244255l;
+		System.out.println(totalhouses);
+	float m416bullet = 5.56f;
+		System.out.println(m416bullet);
+	double lastzone = 32.2446646646;
+		System.out.println(lastzone);
+	char buggycolor = 'r';
+		System.out.println(buggycolor);
+	boolean chickendinner = true;
+		System.out.println(chickendinner);
+}
+}
---
+++ veriables/static.java
@@ -0,0 +1,40 @@
+class vikings
+{
+	static byte noOfseasons = 6;
+
+	static short noOfepisode = 90;
+
+	static int totalstarcaste = 5666;
+
+	static long amountgrossing = 784578457l;
+
+	static float timeofperepisode = 60.5f;
+
+	static double totalseasontime = 55.2655656;
+
+	static char seriesname = 'v';
+
+	static boolean herodies = true;
+
+
+	public static void main(String []args)
+{
+
+	System.out.println(noOfseasons);
+
+	System.out.println(noOfepisode);
+
+	System.out.println(totalstarcaste);
+
+	System.out.println(amountgrossing);
+
+	System.out.println(timeofperepisode);
+
+	System.out.println(totalseasontime);
+
+	System.out.println(seriesname);
+
+	System.out.println(herodies);
+
+}
+}
