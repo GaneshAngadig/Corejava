a86c99f0b8282f900360481e877647a08a9be805
GaneshAngadig <ganesh.xworkz@gmail.com>
2022-02-25 20:47:52 +0530

    uploading default values

---
+++ defaultvalues/defaultvalue.java
@@ -0,0 +1,23 @@
+class defaultvalue
+{
+	static byte dvbyte;
+	static short dvshort;
+	static int dvint;
+	static long dvlong;
+	static float dvfloat;
+	static double dvdouble;
+	static char dvchar;
+	static boolean dvboolean;
+	
+	public static void main(String []chetu)
+{
+		System.out.println(dvbyte);
+		System.out.println(dvshort);
+		System.out.println(dvint);
+		System.out.println(dvlong);
+		System.out.println(dvfloat);
+		System.out.println(dvdouble);
+		System.out.println(dvchar);
+		System.out.println(dvboolean);
+}
+}
