7738b667a7e176d7ce01938d3bba9db14c0f1e71
GaneshAngadig <ganesh.xworkz@gmail.com>
2022-02-25 20:40:16 +0530

    uploading methods

---
+++ methods/Book.java
@@ -0,0 +1,20 @@
+class Book
+{
+	public static void main(String []args){
+    	printusesofbook();
+	printusesofbook();
+	printusesofbook();
+	printusesofbook();
+	printusesofbook();
+}
+
+
+
+	 static void printusesofbook(){
+	System.out.println("writing");
+ 	System.out.println("Reading");
+	System.out.println("Drwaing");
+
+}
+}
+
---
+++ methods/Car.java
@@ -0,0 +1,12 @@
+class Car{
+ public static void main(String []args)
+{
+	perposeofcars(" BMW ");
+}
+	static void perposeofcars(String model)
+{
+		System.out.println(model + "saferide");
+		System.out.println(model + "relaxdrive");	
+		System.out.println(model + "whitepetrol");
+}
+}
