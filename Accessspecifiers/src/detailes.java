eac4f3a97ce0f0427abe90a8f98dd47f4f4e2e5e
GaneshAngadig <ganesh.xworkz@gmail.com>
2022-03-01 10:22:41 +0530

    uploading Accessspecifiers

---
+++ Accessspecifiers/src/detailes.java
@@ -0,0 +1,11 @@
+
+public class detailes {
+	public static void main(String []args)
+	{
+		library.gameOfthrones("Heroien");
+		library.pages(100);
+		library.book("English",199.99);
+		library.seasons(8);
+	}
+
+}
---
+++ Accessspecifiers/src/library.java
@@ -0,0 +1,20 @@
+
+public class library {
+	static void gameOfthrones(String khaleesi)
+	{
+	System.out.println(khaleesi);
+	}
+	static void pages(int pages)
+	{
+		System.out.println(pages);
+	
+	}
+	public static void book(String language,double price)
+	{
+		System.out.println(language+price);
+	}
+	public static void seasons(int Episodes)
+	{
+		System.out.println(Episodes);
+	}
+}
